18fc36af138a14b8363c44cc0440c526047f1e0e
jarvis0000000 <jarvis0000009@gmail.com>
2026-03-14 19:53:27 +0530

    Implement cart functionality with add and order features

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-function toggleMenu(){
+let cart=[]
 
-let menu=document.getElementById("mobileMenu")
+function addToCart(name,price){
 
-if(menu.style.display==="block"){
-menu.style.display="none"
-}else{
-menu.style.display="block"
+cart.push({name,price})
+
+document.getElementById("cartCount").innerText=cart.length
+
+alert(name+" added to cart")
+
 }
+
+function orderNow(){
+
+if(cart.length===0){
+alert("Cart is empty")
+return
+}
+
+let msg="Food Order:%0A"
+
+cart.forEach(item=>{
+msg+=item.name+" - ₹"+item.price+"%0A"
+})
+
+window.open("https://wa.me/919876543210?text="+msg)
 
 }
